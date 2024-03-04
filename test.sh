30f55fbf27d288c1cad02bc2766ae9a030a30433
Zeus-HackOlympus <vishaljuneja707@gmail.com>
2024-03-03 23:49:05 -0700

    updated test.sh

--- test.sh
+++ test.sh
@@ -4,25 +4,10 @@ i=0
 sum=0
 for file in ./inputs/*; do
     echo -n "$file : "
-    output=$(./encoder $seed $m <"$file" | tail -n 1)
+    output=$(./encoder <"$file" | tail -n 1)
     echo $output $seed $m
     sum=$(python3 -c "print($output + $sum)")
     i=$((i + 1))
 done
 
 echo "average std dev = $(python3 -c "print($sum / $i)") $seed $m"
-
-echo "============================================"
-
-i=0
-sum=0
-
-for file in ./inputs/*; do
-    echo -n "$file : "
-    output=$(./demo_encoder <"$file" | tail -n 1)
-    echo $output
-    sum=$(python3 -c "print($output + $sum)")
-    i=$((i + 1))
-done
-
-echo "average std dev = $(python3 -c "print($sum / $i)")"
